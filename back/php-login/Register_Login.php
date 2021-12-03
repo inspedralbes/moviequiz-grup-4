204856a559f88cfeda3c6935c9db82abbab7a437
a18marcastru <a18marcastru@inspedralbes.cat>
2021-12-03 11:40:43 +0100

    Sin style css

--- back/php-login/Register_Login.php
+++ back/php-login/Register_Login.php
@@ -22,8 +22,6 @@
   <head>
     <meta charset="utf-8">
     <title>Welcome to you WebApp</title>
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
     <?php require 'partials/header.php' ?>
--- back/php-login/signup.php
+++ back/php-login/signup.php
@@ -25,8 +25,6 @@
   <head>
     <meta charset="utf-8">
     <title>SignUp</title>
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
 
